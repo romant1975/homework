a92f0fdaeeb23db41e5f3994959be8144f5c4d06
Roman Tykov <rtykov@ukr.net>
2016-04-15 13:47:39 +0300

    15.04.2016

--- JS5-6/script.js
+++ JS5-6/script.js
@@ -1,28 +1,22 @@
 var currentTime = 0,
     count = 0,
-    doc = document;
-function startCounter() {
-    cDate = new Date ();
-    currentTime = cDate . getTime();
-    count = currentTime - BEGIN_TIME;
+    doc = document,
+    startStopChange = 'Start',
+    pauseTimeDisplay = 0,
+    BEGIN_TIME = 0,
+    on_display,
+    mls =0;
+
+function startOn() { 
+    date = new Date();
+    BEGIN_TIME = date.getTime() - pauseTimeDisplay; 
+    on_display = setInterval("this.view_display()", 10); 
+    startStopChange = ( startStopChange == 'Start' ? 'Pause' : 'Start'); 
+    doc.getElementById('startPause').setAttribute("value", startStopChange); 
 };
 
-var startStopChange = 'Start',
-    pauseTimeDisplay = 0,
-    BEGIN_TIME = 0;
-function startOn() {
-    date = new Date();
-    BEGIN_TIME = date.getTime() - pauseTimeDisplay;
-    on_timer = setInterval("this.startCounter()", 8);
-    on_display = setInterval("this.view_display()", 10);
-    startStopChange = ( startStopChange == 'Start' ? 'Pause' : 'Start');
-    doc.getElementById('startPause').setAttribute("value", startStopChange);
-};
-var on_timer,
-    on_display;
 function startOff() {
     pauseTimeDisplay = currentTime - BEGIN_TIME;
-    clearInterval(on_timer);
     clearInterval(on_display);
     startStopChange = ( startStopChange == 'Start' ? 'Pause' : 'Start');
     doc.getElementById('startPause').setAttribute("value", startStopChange);
@@ -36,13 +30,8 @@ function onOff() {
     }
 };
 
-var mls =0;
 function clearСlock() {
-    startStopChange = 'Start';
-    count = 0;
-    mls =0;
     pauseTimeDisplay = 0;
-    clearInterval(on_timer);
     clearInterval(on_display);
     doc.getElementById('display').value = "00:00:00.";
     doc.getElementById('displayMls').value = "000";
@@ -50,7 +39,10 @@ function clearСlock() {
 
 };
 function view_display() {
-  var t = parseInt(count),
+	cDate = new Date ();
+    currentTime = cDate.getTime();
+    count = currentTime - BEGIN_TIME;
+    var t = parseInt(count),
     milliseconds = Math.floor((t % 1000)) + 000,
     seconds = Math.floor((t / 1000) % 60),
     minutes = Math.floor((t / 1000 / 60) % 60),
