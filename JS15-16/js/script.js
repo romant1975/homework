ccfd07d64a2f6bf60e16ad6d4fdb5a6cd4816557
Roman Tykov <rtykov@ukr.net>
2016-05-15 13:59:35 +0300

    15.05.2016

--- JS15-16/js/script.js
+++ JS15-16/js/script.js
@@ -1,32 +1,56 @@
- 'use strict'
-function GoogleCallback (func, data) {
-    window[func](data);
-}
-var doc = document;
+'use strict'
 $(function(){
-  $('.google').submit(function(){
-    
-     var request = $('#settings').val();
-
-    $.ajax ({
-      url: 'http://ajax.googleapis.com/ajax/services/search/web?v=1.0&key=ABQIAAAACKQaiZJrS0bhr9YARgDqUxQBCBLUIYB7IF2WaNrkYqF0tBovNBQFDtM_KNtb3xQxWff2mI5hipc3lg&rsz=large&q='
-            + request + '&callback=GoogleCallback&context=?',
-      dataType: 'jsonp',
-      success: function(data){
-        var ul = doc.createElement("ul");
-        $.each(data.results, function(i, val){
-          var li = doc.createElement("li");
-          li.innerHTML = '<a href="'+val.url+'" title="'+val.url+'" target="_blank">'+val.title+"</a><p class='links'>"
-                  +val.url+"</p><p class='content'>"+val.content+"</p>";                          
-          ul.appendChild(li);
-        });
-        $('.google_result').html(ul);
-      }
-    });
-  });
+$('div.search-box input[type=text]').on('keydown', function (e) {
+    if (e.which == 13) {
+        $(this).parent().find('input[type=submit]').trigger('click');
+        /*return false;*/
+     }
+});
+$("#search-btn").click(function(){
+$.getJSON("http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?",
+{
+tags: $("#searchtext").val(),
+tagmode: "any",
+format: "json"
+},
+function(data) {
+$.each(data.items, function(i,item){
+$("<img/>").attr("src", item.media.m).prependTo("#results");
+if ( i == 10 ) return false;
+});
+});
+});
 });
 
 
 
+function Human() {
+    this.name = 'Jack';
+    this.age = 35;
+    this.gender = 'male';
+    this.height = 180;
+    this.weigt = 80
+  };
+  
+  Human.prototype.worker = function toWork() {
+    this.job = 'IT company';
+    this.money = '500 $'
+  };
+  
+  Human.prototype.student =  function watchinSerials() {
+    this.study = 'Donetsk National Technical University',
+    this.grant = '700 UAH'
+  }
+  
+  var newWorker = new Human();
+  newWorker.worker();
+  console.log('newWorker.worker()',newWorker);
+  
+  var newStudent = new Human();
+  newStudent.student();
+  console.log('newStudent.student()',newStudent);
 
+  
+ 
+  
 
